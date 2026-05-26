96d1086cfc6666019fd636a9a7c7390b292f76bc
Jona40 <00014620@uca.edu.sv>
2026-05-25 19:53:09 -0600

    feature: entidad de book terminada

---
+++ src/main/java/com/uca/pncsegundoparcialbiblioteca/controller/Book.java
@@ -0,0 +1,33 @@
+package com.uca.pncsegundoparcialbiblioteca.controller;
+
+import jakarta.persistence.*;
+        import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "books")
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false, unique = true)
+    private String isbn;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Genre genre;
+
+    private Integer totalCopies;
+    private Integer availableCopies;
+    private Boolean available;
+    private LocalDate publishedDate;
+    private String description;
+}
